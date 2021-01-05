fc9aed181563622fff381b498de8470f42f3bf8b
Sawyer King <kingsawyer555@gmail.com>
2021-01-04 16:38:33 -0800

    Initial commit

---
+++ src/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="HeaderLinks">
+        <ul className="headerUl">
+          <li className="headerLi">SHOP</li>
+          <li className="headerLi">ART</li>
+          <li className="headerLi">ABOUT</li>
+          <li className="headerLi">CONTACT</li>
+        </ul>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
